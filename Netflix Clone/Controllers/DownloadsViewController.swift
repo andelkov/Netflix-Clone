479dda5a0594a88f38fe0ab37e5bd1dfe9f1b0cb
andelkov <andelkov@gmail.com>
2022-04-20 01:47:30 +0200

    App base setup

---
+++ Netflix Clone/Controllers/DownloadsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DownloadsViewController.swift
+//  Netflix Clone
+//
+//  Created by Anđelko on 19.Apr.22.
+//
+
+import UIKit
+
+class DownloadsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+        
+    }
+    
+
+
+}
--- Netflix Clone/Controllers/UpcomingViewController.swift
+++ Netflix Clone/Controllers/UpcomingViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  UpcomingViewController.swift
 //  Netflix Clone
 //
 //  Created by Anđelko on 19.Apr.22.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class UpcomingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemBackground
     }
-
-
+    
 }
-
